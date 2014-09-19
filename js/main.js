4ed369ba96c1b30ddb5095463bd845207197fe6c
Julian van Riet <vanrietjulian@gmail.com>
2014-09-18 20:18:50 -0700

    Changes for Menu

--- js/main.js
+++ js/main.js
@@ -16,3 +16,13 @@ $('.openAbout').click(function click1() {
 $(window).scroll(function(){
   $(".back").stop().animate({"marginTop": ($(window).scrollTop()) + "px", "marginLeft":($(window).scrollLeft()) + "px"}, "slow" );
 });
+
+var menu = document.getElementById('links');
+
+window.onload=function(){
+	addEventListener('click', menu, false);
+}
+
+function menu () {
+	menu.style="display: inline-block";
+}
