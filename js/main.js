1e4afa8afad8f58b0e583affbb5eeaed0cdb87bc
Julian van Riet <vanrietjulian@gmail.com>
2014-09-24 17:30:11 -0700

    Partially Fixed Overlay

--- js/main.js
+++ js/main.js
@@ -11,20 +11,12 @@ $('.openAbout').click(function click1() {
     	$('#popupContent').fadeOut('slow');   
 	});
 
-// $(window).scroll(function(){
-//   $(".back").stop().animate({"marginTop": ($(window).scrollTop()) + "px", "marginLeft":($(window).scrollLeft()) + "px"}, "slow" );
-// });
+$(window).scroll(function(){
+  $("#head2").stop().animate({"marginTop": ($(window).pageTop()) + "px", "marginLeft":($(window).scrollLeft()) + "px"}, "slow" );
+});
 
 $(".hammenu").click(function(){
    $(".links").removeClass('.displaynone');
 });
 
-var more = document.getElementById("more");
 
-window.onload=function(){
-	more.addEventListener('click', textChange, false);
-};
-
-function textChange(){
-	document.getElementByClass("aboutme").innerHTML="I am a comic book lover. ";
-}
