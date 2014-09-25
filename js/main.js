e4c4c3e2568f1bef66f4926ff271ad6a05d46081
Julian van Riet <vanrietjulian@gmail.com>
2014-09-25 07:24:53 -0700

    Added Content to Webpage

--- js/main.js
+++ js/main.js
@@ -11,6 +11,33 @@ $('.openAbout').click(function click1() {
     	$('#popupContent').fadeOut('slow');   
 	});
 
+	$('.openGal').click(function click1() {
+	$('#overlay').fadeIn('slow');
+	$('#popupBox2').fadeIn('slow');
+	$('#popupContent2').fadeIn('slow');    
+});
+
+
+$('#overlay, .closeoverlay').click(function () {
+		$('#overlay').fadeOut('slow');
+		$('#popupBox2').fadeOut('slow');
+		$('#popupContent2').fadeOut('slow');   
+});
+
+$('.openProj').click(function click1() {
+	$('#overlay').fadeIn('slow');
+	$('#popupBox3').fadeIn('slow');
+	$('#popupContent3').fadeIn('slow');    
+});
+
+
+$('#overlay, .closeoverlay').click(function () {
+		$('#overlay').fadeOut('slow');
+		$('#popupBox3').fadeOut('slow');
+		$('#popupContent3').fadeOut('slow');   
+});
+
+
 $(window).scroll(function(){
   $("#head2").stop().animate({"marginTop": ($(window).pageTop()) + "px", "marginLeft":($(window).scrollLeft()) + "px"}, "slow" );
 });
