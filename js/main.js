82c401b5b1c179da3bddd6b658fdecf2525c4a6f
Julian van Riet <vanrietjulian@gmail.com>
2014-09-25 16:47:12 -0700

    Fixed Overlay Issues

--- js/main.js
+++ js/main.js
@@ -8,10 +8,13 @@ $('.openAbout').click(function click1() {
 	$('#overlay, .closeoverlay').click(function () {
 		$('#overlay').fadeOut('slow');
 		$('#popupBox').fadeOut('slow');
-    	$('#popupContent').fadeOut('slow');   
+    	$('#popupContent').fadeOut('slow');
+   
 	});
-
 	$('.openGal').click(function click1() {
+	$('#overlay').fadeOut('slow');
+	$('#popupBox').fadeOut('slow');
+    $('#popupContent').fadeOut('slow');
 	$('#overlay').fadeIn('slow');
 	$('#popupBox2').fadeIn('slow');
 	$('#popupContent2').fadeIn('slow');    
@@ -25,6 +28,9 @@ $('#overlay, .closeoverlay').click(function () {
 });
 
 $('.openProj').click(function click1() {
+	$('#overlay').fadeOut('slow');
+	$('#popupBox2').fadeOut('slow');
+	$('#popupContent2').fadeOut('slow'); 
 	$('#overlay').fadeIn('slow');
 	$('#popupBox3').fadeIn('slow');
 	$('#popupContent3').fadeIn('slow');    
@@ -46,4 +52,15 @@ $(".hammenu").click(function(){
    $(".links").removeClass('.displaynone');
 });
 
-
+function loadplaylist() {
+         current++;
+         var songs = Array("../Portfolio 2.0/music/charliebrown.m4a", "../Portfolio 2.0/music/dontpanic.mp3", "../Portfolio 2.0/music/lifeintechnicolor.m4a", "../Portfolio 2.0/music/askyfullofstars.m4a", '../Portfolio 2.0/music/magic.m4a');
+         playlist = songs.length;
+         if (current == playlist) {
+           //do nothing or stop
+         } else {
+           this.playOptions = document.getElementById("audio");
+           this.playOptions.src = songs[current];
+           this.playOptions.play();
+         }
+     }
